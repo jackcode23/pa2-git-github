669a9b08e04e5f07cfed371f7c0ce74aab1f8ac3
leonorurquizo <urquizochainaleonor@gmail.com>
2025-09-14 15:40:23 -0500

    Filtrado de imagenes por barra de busqueda

--- main.js
+++ main.js
@@ -20,5 +20,23 @@ window.addEventListener('load', () => {
     });
   });
 })
+// Agregamos el listener para la barra de busqueda
+	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+		const busqueda = evento.target.value;
+		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+	});
+
+	// Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
 
 
